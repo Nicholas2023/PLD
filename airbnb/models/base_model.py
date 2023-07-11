48521a21a9a59ba1f0c4257fa85ca3748b18981e
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-07-11 10:21:49 +0300

    task4

--- airbnb/models/base_model.py
+++ airbnb/models/base_model.py
@@ -15,7 +15,7 @@ class BaseModel:
         updated_at (datetime): The datetime when the instance was last updated
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initialize a new instance of BaseModel
         Attributes:
@@ -23,10 +23,25 @@ class BaseModel:
             using `uuid.uuid4()`
             created_at (datetime): Set to current datetime
             updated_at (datetime): Set to current datetime
+        Args:
+            *args: Variable length arg list
+            **kwargs: Arbitrary keyword arg containing attributes names
+            and their values
         """
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+        if not kwargs:
+            self.id = str(uuid4())
+            self.created_at = self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ('created_at', 'updated_at'):
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
 
     def __str__(self):
         """
